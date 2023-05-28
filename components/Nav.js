c9035a528771f9d722c4567bcd5f012e7fc3262d
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-05-28 16:18:29 +0300

    added

--- components/Nav.js
+++ components/Nav.js
@@ -2,18 +2,21 @@
 import Link from 'next/link';
 
 // icons
-import { HiHome, HiBriefcase, HiViewColumns } from 'react-icons/hi2';
+import { HiHome, HiUser, HiBriefcase, HiViewColumns } from 'react-icons/hi2';
 
 const Nav = () => {
   return (
     // nav
     <nav className='flex flex-col items-center justify-center gap-y-4 fixed right-[2%] z-10 top-0 w-16 h-screen'>
       {/* nav inner */}
-      <div className='flex flex-col items-center justify-center gap-y-4 w-full h-[50vh] bg-black border border-accent text-white text-xl rounded-full'>
+      <div className='flex flex-col items-center justify-center gap-y-6 w-full h-max py-4 bg-black border border-accent text-white text-xl rounded-full'>
         <Link href={'/'}>
           <HiHome />
         </Link>
         <Link href={'/about'}>
+          <HiUser />
+        </Link>
+        <Link href={'/resume'}>
           <HiBriefcase />
         </Link>
         <Link href={'/work'}>
---
+++ pages/resume/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Resume = () => {
+  return <div className='h-full bg-base'>resume</div>;
+};
+
+export default Resume;
