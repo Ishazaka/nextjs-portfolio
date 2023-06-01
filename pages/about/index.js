98b0c15f051bf73173b3d5ae4b132e0240d29afc
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-06-01 22:05:23 +0300

    added

--- pages/about/index.js
+++ pages/about/index.js
@@ -7,6 +7,7 @@ import { fadeIn } from '../../variants';
 
 // react countup
 import CountUp from 'react-countup';
+import ProjectsBtn from '../../components/ProjectsBtn';
 
 const About = () => {
   return (
@@ -26,7 +27,46 @@ const About = () => {
           alt=''
         />
       </motion.div>
-      <div className='container mx-auto h-full'>container</div>
+      <div className='container mx-auto h-full flex flex-col xl:flex-row gap-x-4'>
+        <div className='flex-1 xl:pt-12'>
+          <h1 className='h1'>
+            Captivating <span className='text-accent'>stories</span> birth
+            magnificent designs.
+          </h1>
+          <p className='max-w-[700px] mb-12'>
+            10 years ago, I began freelancing as a developer. Since then, I've
+            done remote work for agencies, consulted for startups, and
+            collaborated on digital products for business and consumer use. With
+            quiet confidence, curiosity, and a focus on continual improvement, I
+            tackle development challenges one at a time.
+          </p>
+          <ProjectsBtn />
+        </div>
+        <div className='flex-1 bg-yellow-50/10'>
+          <div className='flex flex-col justify-center items-stretch h-full w-full bg-yellow-200/10'>
+            <div className='flex'>
+              <div className='flex-1 bg-red-50/10'>
+                <div className='text-5xl font-bold'>12+</div>
+                <div>Years of experience</div>
+              </div>
+              <div className='flex-1 bg-red-50/10'>
+                <div className='text-5xl font-bold'>120+</div>
+                <div>Happy clients</div>
+              </div>
+            </div>
+            <div className='flex'>
+              <div className='flex-1 bg-red-50/10'>
+                <div className='text-5xl font-bold'>12+</div>
+                <div>Projects done</div>
+              </div>
+              <div className='flex-1 bg-red-50/10'>
+                <div className='text-5xl font-bold'>120</div>
+                <div>Awards</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
