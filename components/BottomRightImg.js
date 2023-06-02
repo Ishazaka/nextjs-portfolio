e1d593191c3ba988b12f0e6dcc7e1a1fd9a56b81
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-06-02 19:57:47 +0300

    added

--- components/BottomRightImg.js
+++ components/BottomRightImg.js
@@ -6,7 +6,7 @@ const BottomRightImg = () => {
     <div className='absolute -right-32 -bottom-0 mix-blend-color-dodge'>
       <Image
         src={'/bottom-right-img.png'}
-        width={400}
+        width={350}
         height={272}
         className='translate-z-0'
         alt=''
