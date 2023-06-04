3f3ddb05e53d2ff878475050bce9c5259c651965
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-06-04 13:16:45 +0300

    added

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -8,7 +8,7 @@ const Resume = () => {
         {/* timeline */}
         <div className='h-96'>
           {/* item 1 */}
-          <div className='group relative before:absolute before:w-3 before:h-3 before:bg-secondary before:rounded-full before:hover:bg-accent before:z-10 max-w-4xl after:w-[1px] after:h-full after:absolute after:bg-white/10 after:top-0 after:left-[6px] after:bottom-0 after:z-0 before:transition-all before:duration-300'>
+          <div className='group relative before:absolute before:w-3 before:h-3 before:bg-secondary before:rounded-full before:hover:bg-accent before:z-10 max-w-4xl after:w-[1px] after:h-full after:absolute after:bg-white/10 after:top-0 after:left-[6px] after:bottom-0 after:z-0 before:transition-all before:duration-300 pb-16'>
             <div className='pl-20 h-full'>
               <div className='mb-4 group-hover:text-accent transition-all duration-300'>
                 2020 - Present
@@ -18,9 +18,29 @@ const Resume = () => {
             </div>
           </div>
           {/* item 2 */}
-          <div>2</div>
+          <div className='group relative before:absolute before:w-3 before:h-3 before:bg-secondary before:rounded-full before:hover:bg-accent before:z-10 max-w-4xl after:w-[1px] after:h-full after:absolute after:bg-white/10 after:top-0 after:left-[6px] after:bottom-0 after:z-0 before:transition-all before:duration-300 pb-16'>
+            <div className='pl-20 h-full'>
+              <div className='mb-4 group-hover:text-accent transition-all duration-300'>
+                2018 - 2020
+              </div>
+              <h2 className='text-2xl mb-1'>Frontend Developer</h2>
+              <p className='text-sm mb-6'>Andromeda Design Agency</p>
+            </div>
+            <div className='pl-20 h-full'>
+              <h2 className='text-2xl mb-1'>Web Designer</h2>
+              <p className='text-sm mb-6'>Andromeda Design Agency</p>
+            </div>
+          </div>
           {/* item 3 */}
-          <div>3</div>
+          <div className='group relative before:absolute before:w-3 before:h-3 before:bg-secondary before:rounded-full before:hover:bg-accent before:z-10 max-w-4xl after:w-[1px] after:h-full after:absolute after:bg-white/10 after:top-0 after:left-[6px] after:bottom-0 after:z-0 before:transition-all before:duration-300 pb-16'>
+            <div className='pl-20 h-full'>
+              <div className='mb-4 group-hover:text-accent transition-all duration-300'>
+                2020 - Present
+              </div>
+              <h2 className='text-2xl mb-1'>Frontend Developer</h2>
+              <p className='text-sm'>Andromeda Design Agency</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
