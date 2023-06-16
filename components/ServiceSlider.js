282458164e52de641139f9b1e077a59fb29151fd
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-06-16 14:48:07 +0300

    added

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -65,6 +65,7 @@ const ServiceSlider = () => {
         clickable: true,
       }}
       modules={[FreeMode, Pagination]}
+      className='h-[240px] sm:h-[340px]'
     >
       {serviceData.map((item, index) => {
         return (
--- pages/services/index.js
+++ pages/services/index.js
@@ -21,29 +21,40 @@ const Services = () => {
       <BottomRightImg />
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-x-8'>
-          <div className='flex xl:w-[30vw] flex-col text-center lg:text-left mb-4'>
+          <div className='flex xl:w-[30vw] flex-col text-center lg:text-left mb-4 xl:mb-0'>
             <motion.h2
               variants={fadeIn('up', 0.2)}
               initial={'hidden'}
               animate={'show'}
               exit={'hidden'}
-              className='h2'
+              className='h2 xl:mt-8'
             >
               My services<span className='text-accent'>.</span>
             </motion.h2>
-            <motion.p className='mb-4 max-w-xl mx-auto lg:mx-0'>
+            <motion.p
+              variants={fadeIn('up', 0.3)}
+              initial={'hidden'}
+              animate={'show'}
+              exit={'hidden'}
+              className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+            >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
-              minima quae fuga ipsum voluptatem adipisci aut corporis dolore
-              quibusdam assumenda.
+              minima quae fuga ipsum voluptatem adipisci.
             </motion.p>
           </div>
-          <div className='w-full xl:max-w-[60%]'>
+          <motion.div
+            variants={fadeIn('down', 0.5)}
+            initial={'hidden'}
+            animate={'show'}
+            exit={'hidden'}
+            className='w-full xl:max-w-[65%]'
+          >
             <ServiceSlider />
-          </div>
+          </motion.div>
         </div>
       </div>
       {/* bulb */}
-      <div className='absolute -left-32 -bottom-12 rotate-12 mix-blend-color-dodge duration-75  animate-pulse'>
+      <div className='absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge duration-75  animate-pulse'>
         <Image src={'/bulb.png'} width={270} height={200} />
       </div>
     </div>
