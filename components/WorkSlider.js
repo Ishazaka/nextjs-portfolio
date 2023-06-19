76c249e12866a1f39a8aa96f4ff7652f1c66da11
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-06-19 11:03:46 +0300

    added

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -70,16 +70,22 @@ const WorkSlider = () => {
       {workSlides.slides.map((slide) => {
         return (
           <SwiperSlide>
-            <div className='grid grid-cols-2 grid-rows-2 gap-4'>
+            <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
               {slide.images.map((image) => {
                 return (
-                  <div className='relative rounded-lg overflow-hidden'>
-                    <div className='absolute top-0 left-0 w-full h-full z-20'></div>
-                    <Image src={image.path} width={500} height={300} alt='' />
-                    {/* circle */}
-                    <div className='absolute z-30 top-12 left-12 bg-red-500'>
-                      circle
-                    </div>
+                  <div className='relative rounded-lg overflow-hidden group flex justify-center items-center'>
+                    <Image
+                      className='group-hover:scale-110 transition-all duration-300'
+                      src={image.path}
+                      width={500}
+                      height={300}
+                      alt=''
+                    />
+                    {/* overlay */}
+                    <div className='absolute top-0 bottom-0 left-0 right-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-all duration-500'></div>
+                    <button className='absolute bg-accent px-4 py-2 rounded-md scale-125 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-700 text-[15px]'>
+                      Live Project
+                    </button>
                   </div>
                 );
               })}
