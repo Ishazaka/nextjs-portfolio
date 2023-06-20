a8e0ecf6c4099e4e93c61f719f678679e6c2efdb
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-06-20 15:40:38 +0300

    added

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -16,15 +16,15 @@ import { FaQuoteLeft } from 'react-icons/fa';
 const testimonialData = [
   {
     image: '',
-    name: '',
-    position: '',
+    name: 'Jonathan Smith',
+    position: 'Customer',
     message:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
   },
   {
     image: '',
-    name: '',
-    position: '',
+    name: 'Jonathan Smith',
+    position: 'Customer',
     message:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
   },
@@ -36,20 +36,28 @@ const TestimonialSlider = () => {
       {testimonialData.map((person, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className='flex flex-col md:flex-row gap-x-12 h-full px-16'>
+            <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
               {/* avatar, name, position */}
-              <div className='w-full max-w-[300px] flex xl:justify-center items-center relative'>
-                {/* avatar */}
-                <div className='w-[120px] h-[120px] bg-pink-50/10 rounded-full'></div>
-                {/* name */}
-                <div>{person.name}</div>
+              <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+                <div className='flex flex-col justify-center text-center'>
+                  {/* avatar */}
+                  <div className='w-[120px] h-[120px] bg-pink-50/10 rounded-full mb-3 mx-auto'></div>
+                  {/* name */}
+                  <div className='text-lg'>{person.name}</div>
+                  {/* position */}
+                  <div className='text-[12px] uppercase font-extralight tracking-widest mb-4 md:mb-0'>
+                    {person.position}
+                  </div>
+                </div>
               </div>
               {/* quote & message */}
-              <div className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/50 xl:before:absolute xl:before:left-0 xl:before:h-[260px] relative xl:pl-12'>
+              <div className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[260px] relative xl:pl-20'>
                 <div className='mb-4'>
-                  <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20' />
+                  <FaQuoteLeft className='text-4xl xl:text-6xl  text-white/20 mx-auto md:mx-0' />
                 </div>
-                <div className='xl:text-xl text-left'>{person.message}</div>
+                <div className='xl:text-xl text-center md:text-left'>
+                  {person.message}
+                </div>
               </div>
             </div>
           </SwiperSlide>
