30424165647e47b411cc2b85ad0492cde45ab3ab
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-06-21 07:50:16 +0300

    added

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,3 +1,5 @@
+import { BsArrowRight } from 'react-icons/bs';
+
 const Contact = () => {
   return (
     <div className='h-full bg-primary/30'>
@@ -6,34 +8,22 @@ const Contact = () => {
           {/* text */}
           <div className='flex-1'>
             <h2 className='h2 text-center mb-12'>
-              Let's connect <span className='text-accent'>.</span>
+              Let's connect today <span className='text-accent'>.</span>
             </h2>
           </div>
           {/* form */}
-          <form className='flex-1 flex flex-col gap-4 w-full mx-auto'>
-            <div className='flex gap-x-4 w-full'>
-              <input
-                type='text'
-                placeholder='name'
-                className='w-full h-[50px] rounded-md pl-4 capitalize'
-              />
-              <input
-                type='email'
-                placeholder='email'
-                className='w-full h-[50px] rounded-md pl-4 capitalize'
-              />
+          <form className='flex-1 flex flex-col gap-6 w-full mx-auto'>
+            <div className='flex gap-x-6 w-full'>
+              <input type='text' placeholder='name' className='input' />
+              <input type='email' placeholder='email' className='input' />
             </div>
-            <input
-              type='text'
-              placeholder='subject'
-              className='w-full h-[50px] rounded-md pl-4 capitalize'
-            />
-            <textarea
-              placeholder='message'
-              className='w-full h-[50px] rounded-md pl-4 capitalize'
-            ></textarea>
-            <button className='w-full bg-accent h-[50px] rounded-md'>
-              Let's talk
+            <input type='text' placeholder='subject' className='input' />
+            <textarea placeholder='message' className='textarea'></textarea>
+            <button className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center group overflow-hidden hover:border-accent'>
+              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>
+                Let's talk
+              </span>
+              <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
             </button>
           </form>
         </div>
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -2,7 +2,7 @@ import TestimonialSlider from '../../components/TestimonialSlider';
 
 const Testimonials = () => {
   return (
-    <div className='h-full bg-primary/30 py-32 text-white text-center xl:text-left'>
+    <div className='h-full bg-primary/30 py-32 text-white text-center'>
       <div className='container mx-auto h-full flex flex-col justify-center'>
         <h2 className='h2'>
           What clients say <span className='text-accent'>.</span>
