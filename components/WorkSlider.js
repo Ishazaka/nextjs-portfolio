baf8ee2225d0f6b6345de2cacb8ec105c3558d71
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-06-21 11:39:27 +0300

    added

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -92,13 +92,13 @@ const WorkSlider = () => {
                       {/* title */}
                       <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-20 transition-all duration-300'>
                         <div className='flex items-center gap-x-2'>
-                          <div className='text-[13px] tracking-[0.2em]'>
+                          <div className='text-[13px] tracking-[0.2em] delay-100'>
                             LIVE
                           </div>
-                          <div className='text-[13px] tracking-[0.2em] translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-100'>
+                          <div className='text-[13px] tracking-[0.2em] translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
                             PROJECT
                           </div>
-                          <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
+                          <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
                             <BsArrowRight />
                           </div>
                         </div>
