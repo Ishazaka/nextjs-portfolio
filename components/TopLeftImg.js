bcf30c93108befd37fd6e1e2c1c74f7a82b8a128
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-06-22 11:05:10 +0300

    added

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const TopLeftImg = () => {
   return (
-    <div className='absolute left-0 top-0 mix-blend-color-dodge z-10 w-[400px] opacity-50'>
+    <div className='absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50'>
       <Image src='/top-left-img.png' width={400} height={400} alt='' />
     </div>
   );
