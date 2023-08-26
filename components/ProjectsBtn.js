4f2d59f1ff9bc02ac493a739fda1598c6b27c8c7
cristianmihai01 <dev.cristianmihai@gmail.com>
2023-08-26 06:15:21 +0300

    added

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -9,7 +9,7 @@ import { HiArrowRight } from 'react-icons/hi2';
 
 const ProjectsBtn = () => {
   return (
-    <div className='mx-auto xl:mx-0'>
+    <div className='mx-auto xl:mx-0 z-10'>
       <Link
         href={'/work'}
         className='relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group'
